025deb85b4df223feab94de29575a7412117c0ec
Wei Huang <weimpromptu@gmail.com>
2021-06-11 17:08:22 -0400

    part 4

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -6,8 +6,8 @@ class BusinessList extends React.Component {
   render() {
     return (
       <div className="BusinessList">
-        {this.props.businesses.map((business, i) => (
-          <Business business={business} key={i} />
+        {this.props.businesses.map((business) => (
+          <Business business={business} key={business.id} />
         ))}
       </div>
     );
---
+++ src/util/Yelp.js
@@ -0,0 +1,39 @@
+const apiKey =
+  'e2uPvmhWiCX3jo6yqx4282UVFvPNnB7DUn0xnOBpNqwVLnz70w3VmlGmBYzd4JsUgddKQR60vR1GOHLQBg1D4oHhwUAoN4HWWPFl-BuLyheB5m2KLvLZbI_SpKLDYHYx';
+const Yelp = {
+  search(term, location, sortBy) {
+    return fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+      }
+    )
+      .then((response) => {
+        response.json();
+      })
+      .then((jsonResponse) => {
+        console.log('jsonResponse:', jsonResponse);
+        if (jsonResponse.businesses) {
+          return jsonResponse.businesses.map((business) => {
+            console.log(business);
+            return {
+              id: business.id,
+              imageSrc: business.image_url,
+              name: business.name,
+              address: business.location.address1,
+              city: business.location.city,
+              state: business.location.state,
+              zipCode: business.location.zip_code,
+              category: business.categories[0].title,
+              rating: business.rating,
+              reviewCount: business.review_count,
+            };
+          });
+        }
+      });
+  },
+};
+
+export default Yelp;
